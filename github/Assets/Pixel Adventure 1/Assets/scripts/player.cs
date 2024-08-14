641351a880b8a662f47fa16f951a530772c0c1af
maricmara <camara.mariana@escolar.ifrn.edu.br>
2024-08-14 14:37:10 -0300

    fase

--- github/Assets/Pixel Adventure 1/Assets/scripts/player.cs
+++ github/Assets/Pixel Adventure 1/Assets/scripts/player.cs
@@ -12,12 +12,14 @@ public class player : MonoBehaviour
     public bool doubleJump;
 
     private Rigidbody2D rig;
+    private Animator anim;
     
 
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -31,13 +33,30 @@ public class player : MonoBehaviour
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * Speed;
+
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            anim.SetBool("walker", true);
+            transform.eulerAngles = new Vector3(0f,0f,0f);
+        }
+
+        if(Input.GetAxis("Horizontal") < 0f)
+        {
+            anim.SetBool("walker", true);
+            transform.eulerAngles = new Vector3(0f,180f,0f);
+        }
+
+        if(Input.GetAxis("Horizontal") == 0)
+        {
+            anim.SetBool("walker", false);
+        }
     }
 
     void Jump()
     {
         if(Input.GetButtonDown("Jump"))
         {
-            if(isJumping)
+            if(isJumping == false)
             {
                 rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
                 doubleJump = true;
@@ -65,6 +84,9 @@ public class player : MonoBehaviour
 
     void OnCollisionExit2D(Collision2D collision)
     {
-        isJumping = true;
+       if(collision.gameObject.layer ==8)
+        {
+            isJumping = true;
+        }
     }
 }
